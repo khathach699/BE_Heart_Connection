62310f5b91ebbdbe25098e144329507a8e2ec374
KhaThach <khathach699@gmail.com>
2025-04-12 19:29:06 +0700

    fix user

---
+++ src/controllers/userController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import {
+  CreateErrorResponse,
+  CreateSuccessResponse,
+} from "../utils/responnseHandler";
+
+export class UserController {
+  // Add controller methods here
+}
+
+export default new UserController();
